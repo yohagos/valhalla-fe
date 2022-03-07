98f8e1989b9f71a77d7ce2113117e4470c68c51e
yohagos <yosef.hagos@googlemail.com>
2022-03-07 20:49:21 +0100

    initial project - httpServices not working bd of CORS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'valhalla-fe';
+  first: string = ""
+  second: string = ""
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get("localhost:8888").subscribe(
+      data => {
+        this.first = data.toString()
+      }
+    );
+    this.http.get("localhost:8888/post").subscribe(
+      data => {
+        this.second = data.toString()
+      }
+    )
+  }
 }
